110b061655fd19730ed36428cfb0219f45df7e1e
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2022-05-11 18:45:52 +0530

    connect

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+#
+
 
 def launchBrowser():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
