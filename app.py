9b062466a51788d3deb96c48377be023480d8f7c
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2022-05-12 21:02:30 +0530

    .env file created

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import dotENv
-#
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def launchBrowser():
@@ -15,11 +17,14 @@ def launchBrowser():
 
 
 browser = launchBrowser()
+LINKEDIN_PASSWORD = os.getenv('LINKEDIN_PASSWORD')
 
 username = browser.find_element(By.ID, "session_key")
 password = browser.find_element(By.ID, "session_password")
 username.send_keys("mohitkumar11725@gmail.com")
-password.send_keys("LINKEDIN_PASSWORD")
+password.send_keys(LINKEDIN_PASSWORD)
 login = browser.find_element(By.CLASS_NAME, "sign-in-form__submit-button")
 login.click()
+
+
 # driver.get("https://www.linkedin.com/jobs/")
