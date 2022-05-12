6122f814d2253f05abe6f3e9dfd00ff1a79716cf
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2022-05-12 20:52:00 +0530

    python-dotenv installed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
+import dotENv
 #
 
 
@@ -19,7 +19,7 @@ browser = launchBrowser()
 username = browser.find_element(By.ID, "session_key")
 password = browser.find_element(By.ID, "session_password")
 username.send_keys("mohitkumar11725@gmail.com")
-password.send_keys(LINKEDIN_PASSWORD)
+password.send_keys("LINKEDIN_PASSWORD")
 login = browser.find_element(By.CLASS_NAME, "sign-in-form__submit-button")
 login.click()
-driver.get("https://www.linkedin.com/jobs/")
+# driver.get("https://www.linkedin.com/jobs/")
